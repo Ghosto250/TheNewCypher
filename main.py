e93e8d33e6fa4849ec3d6355ff5ac0744fadc78f
GitHub <noreply@github.com>
2022-05-23 16:23:05 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+error = EnvironmentError("help")
+Andrewsdeath = [9*12,+2000],[3.14 * 9]
+life = 32.92604
+death = 0.314 
+deaths = [death * 2],[death * 3],[death * 4]
+anon = [1],[2],['on'],['Live'],['and'],['Me'],['Kill']
+time = Andrewsdeath[0],life
+times = time * 9
+deaths = death * 3.14
+endless = deaths / 9
+ping = ascii.__call__
+life = 32.92604 # Albert Einstein Died at 30 roughly
+lifes = [times * 217],[times * 309],[times * 429]
+x = [life],['Getting all APIS',ping,times,lifes[1]]
+lonely_soul = 404
+age = lonely_soul
+timeod = ("8:45am Sept 3 2022");
+end = reversed(timeod)
+hack = [globals()]
+#Head
+
+life = [{'Life'}]
+twitch,connect,to = ['52.32.76.121::4404'],['52.32.76.121 4768'],['52.32.76.121 4251']
+robinhood,ping,sad,lives = ['99.84.166.52::246'],[True],['Life is a gamble.'],['Living causes immense amount of pain to ones soul']
+lb=10
+activate = True
+worker_threads = lb
+worker = lb * 30 / 3.1
+__doc__ = [worker_threads* 20 / 3.14],[lb * 2004 / 113]
+
+#Body
+
+Andrewsdeath[0] # 104.86
+Andrewsdeath[1] # 28.26#2+8 = 1 # 2 + 6 = 8 or 00 in computer brain
+
+pi = 3.14 / 2 
+p = pi / 2
+pie = p - pi
+
+ # 0 , 1 , 2 (3 options)
+lives = [lifes,pi,worker,__doc__,activate,lb]
+#Feet
+Andrew =  1
+input('Enter name: ')
+print(lonely_soul)
+print('lonely_soul')
+print(__doc__)
+rst = end,reversed(x),reversed(timeod),reversed(lives)
+kill=end,reversed(connect),reversed(robinhood),reversed(sad),reversed(twitch),reversed(connect),reversed(to)
+dict = {'hello'}
+print(kill[0])
+api = repr(rst)
+print(api)
+__doc__
+__file__
+__package__
